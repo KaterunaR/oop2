60b5ad0f6888f8e5d7fdd9b31a47b12896922cb1
KaterunaR <131966803+KaterunaR@users.noreply.github.com>
2024-02-28 22:53:32 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyMatrix
+{
+    public static int col = 5;
+    public static int row = 5;
+    public int[,] matrix = new int[row, col];
+    public int RowCount { get; set; } = row;
+    public int ColCount { get; set; } = col;
+
+    public void FillMatrix()
+    {
+        Random random = new Random();
+        for (int i = 0; i < RowCount; i++)
+        {
+            for (int j = 0; j < ColCount; j++)
+            {
+                matrix[i, j] = random.Next(-5, 10);
+            }
+        }
+    }
+
+    public int FirstColumnWithoutNegativeElement()
+    {
+        for (int j = 0; j < ColCount; j++)
+        {
+            bool columnHasNegative = false;
+            for (int i = 0; i < RowCount; i++)
+            {
+                if (matrix[i, j] < 0)
+                {
+                    columnHasNegative = true;
+                    break;
+                }
+            }
+            if (!columnHasNegative)
+            {
+                return j; 
+            }
+        }
+        return -1; 
+    }
+
+    public void SortRowsByCountOfEqualElements()
+    {
+        List<KeyValuePair<int, int>> rowCounts = new List<KeyValuePair<int, int>>();
+        for (int i = 0; i < RowCount; i++)
+        {
+            int count = CountEqualElementsInRow(i);
+            rowCounts.Add(new KeyValuePair<int, int>(i, count));
+        }
+
+        rowCounts.Sort((x, y) => x.Value.CompareTo(y.Value));
+
+        int[,] sortedMatrix = new int[RowCount, ColCount];
+        for (int i = 0; i < RowCount; i++)
+        {
+            for (int j = 0; j < ColCount; j++)
+            {
+                sortedMatrix[i, j] = matrix[rowCounts[i].Key, j];
+            }
+        }
+
+        matrix = sortedMatrix; 
+    }
+
+    private int CountEqualElementsInRow(int rowIndex)
+    {
+        HashSet<int> uniqueElements = new HashSet<int>();
+        for (int j = 0; j < ColCount; j++)
+        {
+            uniqueElements.Add(matrix[rowIndex, j]);
+        }
+        return uniqueElements.Count;
+    }
+}
+
+
+
---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using lab1;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        MyMatrix matrixObject = new MyMatrix();
+
+        public Form1()
+        {
+            InitializeComponent();
+            matrixObject.FillMatrix();
+            DisplayMatrixInGridView();
+        }
+
+        private void DisplayMatrixInGridView()
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+            for (int i = 0; i < matrixObject.RowCount; i++)
+            {
+                dataGridView1.Columns.Add("Колонка" + i, "Колонка" + i);
+            }
+            for (int i = 0; i < matrixObject.RowCount; i++)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+
+                for (int j = 0; j < matrixObject.ColCount; j++)
+                {
+                    row.Cells.Add(new DataGridViewTextBoxCell { Value = matrixObject.matrix[i, j] });
+                }
+
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            matrixObject.FillMatrix();
+            DisplayMatrixInGridView();
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int columnWithoutNegative = matrixObject.FirstColumnWithoutNegativeElement();
+            if (columnWithoutNegative != -1)
+            {
+                textBox3.Text = (columnWithoutNegative).ToString(); 
+            }
+            else
+            {
+                textBox3.Text = "такої немає";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            matrixObject.SortRowsByCountOfEqualElements();
+            DisplayMatrixInGridView();
+        }
+    }
+}
+
